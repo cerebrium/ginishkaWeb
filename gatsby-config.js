f812ee22ac162ab7495d6dae69e9ce1f3b37162d
cerebrium <nicholas.m.shankland@gmail.com>
2019-12-29 13:31:45 -0800

    changed starting route

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  pathPrefix: "/ginishkaWeb",
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
@@ -20,7 +21,7 @@ module.exports = {
       options: {
         name: `gatsby-starter-default`,
         short_name: `starter`,
-        start_url: `/`,
+        start_url: `/about/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
